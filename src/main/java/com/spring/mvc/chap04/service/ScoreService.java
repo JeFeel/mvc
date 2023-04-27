b097a364a235168c2538c951ae31711160196770
JeFeel <jaephil94@gmail.com>
2023-04-27 12:13:04 +0900

    Spring JDBC 방식으로 변경

--- src/main/java/com/spring/mvc/chap04/service/ScoreService.java
+++ src/main/java/com/spring/mvc/chap04/service/ScoreService.java
@@ -22,7 +22,7 @@ public class ScoreService {
     private final ScoreRepository scoreRepository;
 
     @Autowired
-    public ScoreService(@Qualifier("jdbc") ScoreRepository scoreRepository) {
+    public ScoreService(@Qualifier("spring") ScoreRepository scoreRepository) {
         this.scoreRepository = scoreRepository;
     }
 
